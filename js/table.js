2b31611c848f53149b9ae00d467d2d0b139b04a7
Julio César Rey Méndez <julio.rey@gmail.com>
2020-04-18 15:29:23 -0400

    firstcommit

---
+++ js/table.js
@@ -0,0 +1,204 @@
+var efectoFinalizado = function(index){
+    console.log('efector finalizado [' , index);
+};
+
+trucoTableRender = function (table) {
+    console.log('render table', table);
+    var svgns = "http://www.w3.org/2000/svg";
+    var container = document.getElementById('table-screen');
+    var radious = 40;
+
+    var H = 400;
+    var K = 250;
+
+
+
+    var getCircle = function(x, y, r, color ){
+        var circle = document.createElementNS(svgns, 'circle');
+        circle.setAttributeNS(null, 'cx', H + x);
+        circle.setAttributeNS(null, 'cy', K + y);
+        circle.setAttributeNS(null, 'r', r);
+        circle.setAttributeNS(null, 'fill', color);
+        //circle.setAttribute('transform',  ' translate(400,250)');
+        return circle;
+    };
+    var getImage = function(imageName, point){
+        var svgimg = document.createElementNS('http://www.w3.org/2000/svg','image');
+        // svgimg.setAttributeNS(null,'height','200');
+        // svgimg.setAttributeNS(null,'width','200');
+        svgimg.setAttributeNS('http://www.w3.org/1999/xlink','href', imageName);
+        svgimg.setAttributeNS(null,'x',H+point.x+ '');
+        svgimg.setAttributeNS(null,'y',K+point.y + '');
+        //svgimg.setAttribute('transform',  ' translate('+point.x+','+point.y+')');
+        // svgimg.setAttributeNS(null,'height','100');
+        // svgimg.setAttribute('transform',  ' translate(400,250)');
+        svgimg.setAttributeNS(null, 'visibility', 'visible');
+        $('svg').append(svgimg);
+        return svgimg;
+    }
+    var getDorsoImage = function(point){
+        return getImage('images/cards/dorso.gif', point);
+    };
+    var getCardImage = function(type, value, points){
+        // Append image to SVG
+        return getImage('images/cards/'+type + '/' + value + '.gif', points);
+    };
+    var setCardImage = function(svgimg, type, value){
+        // Append image to SVG
+        svgimg.setAttributeNS('http://www.w3.org/1999/xlink','href', 'images/cards/'+type + '/' + value + '.gif');
+    };
+
+
+    var cardManager = function (index, circle, x, y, rotation) {
+        var $this = this;
+        this.cards = [];
+        this.rotation = rotation;
+
+        this.path = function () {
+
+
+        };
+
+        this.card = function (data) {
+            var $this = this;
+            $this.data = data;
+            data.type = 'basto';
+            data.value = 1;
+
+
+
+            this.flip = function(){
+
+            };
+
+            this.play = function(){
+                console.log('play!'+index);
+                // setCardImage(card, data.type, data.value);
+                //"rotate("+(data.rotation) +", "+data.x+ ", "+ data.y+")"
+                // data.el.setAttribute("transform",  + ' translate('+(-1*x)+','+(-1*y)+' )');
+                $(animation).get(0).beginElement();
+            }
+
+            var card = getDorsoImage({x:0, y:0});
+            // var card = getCircle(x, y, 20);
+            var $rot = index-1;
+            $(card).addClass('selectable');
+            // Important! Align
+            //+  '
+            //
+            //+ ' rotate(' + rotation + $rot*30+ ")"
+            // + ' rotate(' + rotation + ")" + ' translate('+ (radious * -1 )+','+0+')')
+            // card.setAttribute('transform',  ' translate('+ (x)+','+y+')');
+
+            var animation = document.createElementNS('http://www.w3.org/2000/svg', 'animateMotion');
+            animation.setAttribute('dur', '3s');
+            animation.setAttribute('begin', 'indefinite');
+            animation.setAttribute('repeatCount', '1');
+            animation.setAttribute('fill', 'freeze');
+
+            var mpath = document.createElementNS('http://www.w3.org/2000/svg', 'mpath');
+            mpath.setAttributeNS("http://www.w3.org/1999/xlink", "href", "#path_"+index);
+
+            animation.setAttributeNS(null,"onend", "efectoFinalizado("+index+")");
+
+            animation.appendChild(mpath);
+            card.appendChild(animation);
+            container.append(card);
+            console.log('rotation', data);
+            // card.setAttribute('transform',  ' translate('+x+','+ y + ') ');
+
+            card.addEventListener("click", function () {
+                console.log('flip card');
+                $this.play();
+            });
+
+        };
+
+
+
+        this.addCard = function(index){
+            // var card = getCardImage('basto', '1', {x:x, y:y});
+            console.log('adding card', rotation);
+            $this.cards.push(new $this.card({
+                x : x,
+                y : y,
+                rot : rotation,
+                type : null,
+                value: null,
+                flipped : false,
+                index : index
+            }));
+
+        };
+
+
+
+
+    }
+
+    var pos = {
+        '2': [
+            [0, -240],
+            [0, 240],
+        ],
+        '4': [
+            [0, -200],
+            [200, 0],
+            [0, 200],
+            [-200, 0]
+        ],
+        '6': [
+            [0, -250, 180],
+            [340, -150, 180-65],
+            [340, 150, 65],
+            [0, 250, 0],
+            [-340, 150, -65],
+            [-340, -150, -115]
+        ]
+    };
+
+
+
+    if (table.status == 'NEW') {
+        // Render Table
+    }
+
+
+    this.init = function () {
+        var size = '6';
+        var dis = pos[size];
+        var colors = ['red', 'blue', 'yellow', 'cyan', 'cyan', 'magenta'];
+
+        var cards = [];
+
+        for (var i in dis) {
+            var point1 = {x: dis[i][0], y: dis[i][1]};
+            var point2 = {x: 0, y: 0};
+
+            var rotation = dis[i][2];
+
+
+            // User Circle
+            var circle = getCircle(point1.x, point1.y, radious, colors[i]);
+            container.appendChild(circle);
+
+            cards[i] = new cardManager(i, circle, point1.x, point1.y, rotation);
+            cards[i].addCard(0, {});
+            // cards[i].addCard(1, {});
+            // cards[i].addCard(2, {});
+
+            // User Path
+            var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            path.setAttribute('id', 'path_'+i);
+            path.setAttributeNS(null, "d", 'M' + point1.x + ',' + point1.y +' L ' + H/2 + ',' + K/2);
+            // newLine.setAttribute('c', );
+            path.setAttribute('stroke-width', '1');
+            path.setAttribute('stroke', 'cyan');
+            path.setAttribute('transform',  ' translate(400,250)');
+            $(container).append(path);
+        }
+        // paths
+    };
+    this.init();
+
+};
---
+++ js/toluca.js
@@ -0,0 +1,137 @@
+function TolucaWS(handler){
+    var connection = null;
+
+    var readyStateConstants = {
+        'CONNECTING': 0,
+        'OPEN': 1,
+        'CLOSING': 2,
+        'CLOSED': 3,
+        'RECONNECT_ABORTED': 4
+    };
+
+    this.connect = function(){
+        connection = new WebSocket('ws://localhost:8050/ws?amigo');
+        console.log('connection=',connection);
+
+        connection.onopen = function(event){
+            console.log('connection opened', event);
+            //connection.send(JSON.stringify(obj));
+            handler.ready();
+        };
+        connection.onmessage = function(event) {
+            try{
+                handler.onmessage(JSON.parse(event.data));
+            }catch (e) {
+                console.log('error reading message', e);
+            }
+        };
+
+        this.sendMessage = function(event){
+            connection.send(JSON.stringify(event));
+        };
+        console.log('connection is ok');
+    }
+}
+function TolucaClient(render){
+    var $this = this;
+    var ws = new TolucaWS(this);
+
+    this.ready = function(){
+        $this.getRooms();
+    };
+
+    //Events
+
+    this.onmessage = function(obj){
+        if (obj.type == "COMMAND_RESPONSE"){
+            $this.commandResponse(obj.data);
+        }
+        else if (obj.type == "TRUCO_ROOM_EVENT"){
+            $this.trucoRoomEvent(obj.data);
+        }
+        else{
+            console.log('message not implemented', obj);
+        }
+    };
+    this.commandResponse = function(data){
+        if (data.eventName == 'ROOMS_FOUND'){
+            $this.roomsFound(data.rooms);
+        }
+        else if (data.eventName == 'ROOM_USER_JOINED'){
+            $this.roomFound(data.room);
+        }
+        else {
+            console.log('command not implemented', data);
+        }
+    };
+    this.trucoRoomEvent = function(data){
+        if (data.eventName == 'ROOM_TABLE_USER_JOINED'){
+            $this.roomTableUserJoined(data);
+        }
+        else if (data.eventName == 'ROOM_TABLE_CREATED'){
+            $this.tableCreated(data.table);
+        }
+        else if (data.eventName == 'ROOM_USER_JOINED'){
+            $this.roomUserJoined(data.user);
+        }
+        else {
+            console.log('trucoRoomEvent not implemented', data);
+        }
+    }
+    this.roomsFound = function(rooms){
+        render.updateRooms(rooms);
+    };
+    this.roomFound = function(rooms){
+        render.updateRoom(rooms);
+    };
+    this.tableCreated = function(table){
+        render.tableCreated(table);
+    };
+    this.roomUserJoined = function(user){
+        render.roomUserJoined(user);
+    };
+    this.roomTableUserJoined = function(data){
+        render.roomTableUserJoined(data);
+    };
+
+    //Commans;
+    this.getRooms = function(){
+        ws.sendMessage({
+            "command":"get_rooms"
+        });
+    };
+
+    this.joinRoom = function(roomId){
+        ws.sendMessage({
+            "command":"join_room",
+            "data" : {
+                "roomId" : roomId
+            }
+        });
+    };
+
+    this.createTable = function(roomId){
+        ws.sendMessage({
+            "command":"create_room_table",
+            "data" : {
+                "roomId" : roomId,
+                "points" : 30
+            }
+        });
+
+    };
+
+    this.joinRoomTable = function(roomId, tableId){
+        ws.sendMessage({
+            "command":"join_room_table",
+            "data" : {
+                "roomId" : roomId,
+                "tableId" : tableId,
+            }
+        });
+    };
+
+    ws.connect();
+
+
+}

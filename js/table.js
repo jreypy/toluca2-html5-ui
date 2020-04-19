2a4bacd0f1faae8137e786f026067d0a4b8a059f
Julio César Rey Méndez <julio.rey@gmail.com>
2020-04-19 17:09:52 -0400

    add text to player

--- js/table.js
+++ js/table.js
@@ -1,5 +1,5 @@
-var efectoFinalizado = function(index, event){
-    console.log('efector finalizado [' , index);
+var efectoFinalizado = function (index, event) {
+    console.log('efector finalizado [', index);
     console.log(event);
 };
 
@@ -9,21 +9,40 @@ trucoTableRender = function (context, toluca) {
     console.log('render table', context.table);
     var svgns = "http://www.w3.org/2000/svg";
     var container = document.getElementById('table-screen');
-    var radious = 40;
+
     var table = context.table;
 
     var H = 400;
     var K = 300;
+    var RADIO = 40;
 
 
+    var radious = RADIO;
 
-    var getG = function(){
+
+    var getG = function () {
         var g = document.createElementNS(svgns, 'g');
+        g.setAttributeNS(null, 'width', radious*2);
+        g.setAttributeNS(null, 'height', radious*2);
+        // width="200" height="100"
         //circle.setAttribute('transform',  ' translate(400,250)');
         return g;
     };
 
-    var getCircle = function(x, y, r, color ){
+
+    var addText = function(text){
+        var element = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        element.setAttributeNS(null, 'x', 1);
+        element.setAttributeNS(null, 'y', RADIO);
+        // element.setAttributeNS(null, 'dominant-baseline', 'middle');
+        // element.setAttributeNS(null, 'text-anchor', 'middle');
+        var txt = document.createTextNode(text);
+
+        element.appendChild(txt);
+        return element;
+    }
+
+    var getCircle = function (x, y, r, color) {
         var circle = document.createElementNS(svgns, 'circle');
         circle.setAttributeNS(null, 'cx', x);
         circle.setAttributeNS(null, 'cy', y);
@@ -32,13 +51,13 @@ trucoTableRender = function (context, toluca) {
         //circle.setAttribute('transform',  ' translate(400,250)');
         return circle;
     };
-    var getImage = function(imageName, point){
-        var svgimg = document.createElementNS('http://www.w3.org/2000/svg','image');
+    var getImage = function (imageName, point) {
+        var svgimg = document.createElementNS('http://www.w3.org/2000/svg', 'image');
         // svgimg.setAttributeNS(null,'height','200');
         // svgimg.setAttributeNS(null,'width','200');
-        svgimg.setAttributeNS('http://www.w3.org/1999/xlink','href', imageName);
-        svgimg.setAttributeNS(null,'x', point.x);
-        svgimg.setAttributeNS(null,'y', point.y);
+        svgimg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', imageName);
+        svgimg.setAttributeNS(null, 'x', point.x);
+        svgimg.setAttributeNS(null, 'y', point.y);
         //svgimg.setAttribute('transform',  ' translate('+point.x+','+point.y+')');
         // svgimg.setAttributeNS(null,'height','100');
         // svgimg.setAttribute('transform',  ' translate('+H+','+K+')');
@@ -46,18 +65,17 @@ trucoTableRender = function (context, toluca) {
         $('svg').append(svgimg);
         return svgimg;
     }
-    var getDorsoImage = function(point){
+    var getDorsoImage = function (point) {
         return getImage('images/cards/dorso.gif', point);
     };
-    var getCardImage = function(type, value, points){
+    var getCardImage = function (type, value, points) {
         // Append image to SVG
-        return getImage('images/cards/'+type + '/' + value + '.gif', points);
+        return getImage('images/cards/' + type + '/' + value + '.gif', points);
     };
-    var setCardImage = function(svgimg, type, value){
+    var setCardImage = function (svgimg, type, value) {
         // Append image to SVG
-        svgimg.setAttributeNS('http://www.w3.org/1999/xlink','href', 'images/cards/'+type + '/' + value + '.gif');
+        svgimg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'images/cards/' + type + '/' + value + '.gif');
     };
-
 
 
     var CardsManager = function (index, circle, x, y, rotation) {
@@ -77,11 +95,11 @@ trucoTableRender = function (context, toluca) {
             data.value = 1;
 
 
-            this.flip = function(){
+            this.flip = function () {
 
             };
 
-            this.play = function(){
+            this.play = function () {
                 // console.log('play!'+index);
                 setCardImage(card, data.type, data.value);
                 //"rotate("+(data.rotation) +", "+data.x+ ", "+ data.y+")"
@@ -89,9 +107,9 @@ trucoTableRender = function (context, toluca) {
                 $(animation).get(0).beginElement();
             }
 
-            var card = getDorsoImage({x:H+data.x, y:K-data.y});
+            var card = getDorsoImage({x: H + data.x, y: K - data.y});
             // var card = getCircle(H+data.x, K-data.y, 20);
-            var $rot = data.num-1;
+            var $rot = data.num - 1;
             $(card).addClass('selectable');
             // Important! Align
             //+  '
@@ -107,20 +125,20 @@ trucoTableRender = function (context, toluca) {
             animation.setAttribute('fill', 'freeze');
 
             var mpath = document.createElementNS('http://www.w3.org/2000/svg', 'mpath');
-            mpath.setAttributeNS("http://www.w3.org/1999/xlink", "href", "#path_"+index);
+            mpath.setAttributeNS("http://www.w3.org/1999/xlink", "href", "#path_" + index);
 
-            animation.setAttributeNS(null,"onend", "efectoFinalizado("+index+")");
+            animation.setAttributeNS(null, "onend", "efectoFinalizado(" + index + ")");
 
             animation.appendChild(mpath);
             card.appendChild(animation);
             container.append(card);
             // console.log('data', data);
             //' translate('+H+','+K+')  '
-            var translate  = 'translate('+(radious*-1)+','+0+') '
-            var rotate = ' rotate('+(data.rot+$rot*20) + ','+(H+data.x+radious)+ ','+(K-data.y)+')';
+            var translate = 'translate(' + (radious * -1) + ',' + 0 + ') '
+            var rotate = ' rotate(' + (data.rot + $rot * 20) + ',' + (H + data.x + radious) + ',' + (K - data.y) + ')';
             var transform = translate + ' ' + rotate;
             // console.log('rotation', [data.index,transform]);
-            card.setAttribute('transform',   transform);
+            card.setAttribute('transform', transform);
 
             card.addEventListener("click", function () {
                 console.log('flip card');
@@ -130,68 +148,77 @@ trucoTableRender = function (context, toluca) {
         };
 
 
-
-        this.addCard = function(num){
+        this.addCard = function (num) {
             // var card = getCardImage('basto', '1', {x:x, y:y});
             $this.cards.push(new $this.card({
-                x : x,
-                y : y,
-                rot : rotation,
-                type : null,
+                x: x,
+                y: y,
+                rot: rotation,
+                type: null,
                 value: null,
-                flipped : false,
-                index : index,
-                num : num
+                flipped: false,
+                index: index,
+                num: num
             }));
 
         };
 
 
-
-
     };
 
-    var PlayerManager = function(index, point, circle, rotation){
+    var PlayerManager = function (index, point, rotation) {
         var $this = this;
 
+        var circle = getCircle(radious, radious, radious, 'white');
+        $(circle).addClass('selectable');
+
+        var g = getG();
+
+        var text = addText('Player '+ (index+1));
+
+
+        var translate = 'translate(' + (H + point.x - radious) + ',' + (K - point.y - radious) + ') '
+        g.setAttribute('transform', translate)
 
 
 
         $(circle).click(function () {
             // unselect / select
             circle.setAttributeNS(null, 'fill', 'gray');
-            if (table.status == 'NEW'){
+            if (table.status == 'NEW') {
                 toluca.setTablePosition(table.roomId, context.table.id, index);
-            }else{
+            } else {
                 console.log('position no selected', [index, context.table]);
             }
         });
 
 
-        this.setPlayer = function(player, fire){
+        this.setPlayer = function (player, fire) {
             $this.player = player;
-            if (player == null){
+            if (player == null) {
                 circle.setAttributeNS(null, 'fill', 'gray');
             }
-            else if (fire){
+            else if (fire) {
                 circle.setAttributeNS(null, 'fill', 'red');
-            }else{
+            } else {
                 circle.setAttributeNS(null, 'fill', 'blue');
             }
 
         };
 
         var cardsManager = new CardsManager(index, {}, point.x, point.y, rotation);
+
         cardsManager.addCard(0, {});
         cardsManager.addCard(1, {});
         cardsManager.addCard(2, {});
 
 
+        g.appendChild(circle);
+        g.appendChild(text);
+        container.appendChild(g);
+        return g;
 
     };
-
-
-
 
 
     if (table.status == 'NEW') {
@@ -214,7 +241,7 @@ trucoTableRender = function (context, toluca) {
             ],
             '6': [
                 [0, -250, 180],
-                [340, -150, 180-65],
+                [340, -150, 180 - 65],
                 [340, 150, 65],
                 [0, 250, 0],
                 [-340, 150, -65],
@@ -224,25 +251,22 @@ trucoTableRender = function (context, toluca) {
 
         var dis = pos[size];
 
-        var cards = [];
+        var players = [];
 
-        for (var i in dis) {
+        for (var i =0; i<dis.length; i++) {
+
             var point1 = {x: dis[i][0], y: dis[i][1]};
             var point2 = {x: 0, y: 0};
 
 
-            var circle = getCircle(H + point1.x, K - point1.y, radious, 'white');
-            $(circle).addClass('selectable');
-            container.appendChild(circle);
 
-            $this.players[i] = new PlayerManager(i, point1, circle, dis[i][2]);
-
-
+            var player = $this.players[i] = new PlayerManager(i, point1, dis[i][2]);
+            players[i] = player;
 
             // User Path
             var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-            path.setAttribute('id', 'path_'+i);
-            var d = 'M ' +'0 , 0'+ ' L ' + (point2.x-point1.x-100) + ' ' + (point2.y + point1.y);
+            path.setAttribute('id', 'path_' + i);
+            var d = 'M ' + '0 , 0' + ' L ' + (point2.x - point1.x - 100) + ' ' + (point2.y + point1.y);
             // console.log('d', d);
             path.setAttributeNS(null, "d", d);
             // // newLine.setAttribute('c', );
@@ -253,11 +277,11 @@ trucoTableRender = function (context, toluca) {
         }
         // paths
     };
-    this.setPlayerPosition = function(params){
+    this.setPlayerPosition = function (params) {
         console.log('setting position ', params);
-        for (var i in params.table.positions){
+        for (var i in params.table.positions) {
 
-            $this.players[i].setPlayer(params.table.positions[i], params.index==i);
+            $this.players[i].setPlayer(params.table.positions[i], params.index == i);
         }
     };
     this.size = 6;
